f41b47b127f6c2c5b12755a1d9c2bbe5b9c9bcb1
Luis Felipe <luisfurquijo38@gmail.com>
2024-03-19 11:03:21 -0500

    implementando login y registro

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_normal_user = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            if self.is_normal_user:
+                self.is_staff = False
+                self.is_superuser = False
+            elif self.is_admin:
+                self.is_staff = True
+                self.is_superuser = False
+        super().save(*args, **kwargs)
+
